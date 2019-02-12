61fd506e0e0359adba0872e798f857fa5af75357
Catherine Wang <catherine_wang@berkeley.edu>
2019-02-12 15:09:16 -0800

    filled module function

---
+++ cryptorandom/module.py
@@ -0,0 +1,33 @@
+"""
+Code template.
+
+Here's where you should write a short description of what the module contains.
+
+This file belongs in the cryptorandom directory
+"""
+
+# import stuff from __future__ so Python 2.7 users can run our Python 3 code
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+import numpy as np
+
+# Fill in the following function and documentation template below.
+
+def add(x, y):
+    """
+    Add x and y
+
+    Parameters
+    ----------
+    x : (data type)
+        (description of x)
+    y : (data type)
+        (description of y)
+
+    Returns
+    -------
+    (data type returned)
+        (description of return value)
+    """
+    # core of the function
+    return x + y
