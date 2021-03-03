92c4bf75feab92151e7d5c9f2d7ed135c174486d
Jarrod Millman <jarrod.millman@gmail.com>
2021-03-03 09:30:28 -0800

    Use numpydoc

--- doc/conf.py
+++ doc/conf.py
@@ -28,7 +28,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.viewcode',
     'sphinx.ext.imgmath',
-    'sphinx.ext.napoleon',
+    'numpydoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
